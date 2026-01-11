ea54dc6b8001e04a5d5863d84ed2ed0db6b2843f
tant <2188362+tant@users.noreply.github.com>
2026-01-11 11:15:27 +0700

    [WEB-5472] refactor: components of project creation flow (#8462)

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -51,3 +51,4 @@ export * from "./workspace-draft-issues/base";
 export * from "./workspace-notifications";
 export * from "./workspace-views";
 export * from "./base-layouts";
+export * from "./pagination";
